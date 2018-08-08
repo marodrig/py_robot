ac9a2f6aa8b172f0a2153572f38770c62a13c139
Alexei Rodriguez <marodrig@alumni.purdue.edu>
2018-08-08 17:17:08 -0400

    feat: adds handler for commands.

--- toy_robot/toy_handler.py
+++ toy_robot/toy_handler.py
@@ -1,6 +1,7 @@
 import sys
 from toy_robot.robot_toy import RobotToy
 
+
 class ToyHandler(object):
     def __init__(self):
         self.toy = RobotToy()
@@ -9,4 +10,22 @@ class ToyHandler(object):
         with sys.stdin as py_stdin:
             for line in py_stdin:
                 commands = list(line.split(','))
-                if commands[0] == 'PLACE'
+                if commands[0] == 'PLACE':
+                    self.toy.place_robot(coord_x=commands[1], 
+                                         coord_y=commands[2], 
+                                         dir_facing=commands[3])
+                elif commands[0] == 'MOVE':
+                    self.toy.move_forward()
+                elif commands[0] == 'LEFT':
+                    self.toy.rotate_left()
+                elif commands[0] == 'RIGHT':
+                    self.toy.rotate_right()
+                elif commands[0] == 'REPORT':
+                    print(self.toy)
+                else:
+                    print('Bad command.')
+
+
+if __name__ == '__main__':
+    handler = ToyHandler()
+    handler.read_command()

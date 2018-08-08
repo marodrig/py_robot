d773895ec463bb53db03b970f799793271400217
Alexei Rodriguez <marodrig@alumni.purdue.edu>
2018-08-08 15:48:54 -0400

    feat: adds capacity to place robot.

--- test/test_robot_toy.py
+++ test/test_robot_toy.py
@@ -11,6 +11,27 @@ class TestRobotToy(unittest.TestCase):
         self.toy.direction = 'N'
         self.assertEqual(self.toy.direction, 'N')
 
+    def test_sw_direction(self):
+        with self.assertRaises(ValueError):
+            self.toy.direction = 'SW'
+
+    def test_valid_position(self):
+        self.toy.position.x = 1
+        self.assertEqual(self.toy.position.x, 1)
+
+    def test_invalid_position(self):
+        with self.assertRaises(ValueError):
+            self.toy.position.y = 5
+
+    def test_invalid_placement(self):
+        with self.assertRaises(ValueError):
+            self.toy.place_robot('S', -1, -1)
+
+    def test_move_forward_n_from_origin(self):
+        self.toy.place_robot('N', 0, 0)
+        self.toy.move_forward()
+        self.assertEqual(self.toy.position.y, 1)
+
     def tearDown(self):
         self.toy = None
 
--- toy_robot/robot_toy.py
+++ toy_robot/robot_toy.py
@@ -18,4 +18,22 @@ class RobotToy(object):
         else:
             raise ValueError('Direction can only be N, E, S, or W.')
 
+    @property
+    def position(self):
+        return self._position
+
+    def place_robot(self, dir, coord_x, coord_y):
+        self.direction = dir
+        self.position.x = coord_x
+        self.position.y = coord_y
+
+    def move_forward(self):
+        if self.direction == 'N':
+            self.position.y += 1
+        elif self.direction == 'E':
+            self.position.x += 1
+        elif self.direction == 'S':
+            self.position.y -= 1
+        elif self.direction == 'W':
+            self.position.x -= 1
     

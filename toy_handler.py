a8cb9ffee24d2b13a06d887c42ff3e04830a6b65
Alexei Rodriguez <marodrig@alumni.purdue.edu>
2018-08-08 18:15:24 -0400

    chore: shorter parsing for commands.

--- toy_handler.py
+++ toy_handler.py
@@ -9,11 +9,11 @@ class ToyHandler(object):
     def read_command(self):
         with sys.stdin as py_stdin:
             for line in py_stdin:
-                commands = list([str(x).rstrip().strip(',') for x in line.split(' ')])
+                commands = list([str(x).rstrip() for x in line.split(' ')])
                 if commands[0] == 'PLACE':
-                    self.toy.place_robot(coord_x=commands[1],
-                                        coord_y=commands[2],
-                                        dir_facing=commands[3][0])
+                    self.toy.place_robot(coord_x=commands[1].strip(','),
+                                         coord_y=commands[2].strip(','),
+                                         dir_facing=commands[3][0])
                 elif commands[0] == 'MOVE':
                     self.toy.move_forward()
                 elif commands[0] == 'LEFT':
